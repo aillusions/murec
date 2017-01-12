5b165a194998e4c57749b56cbf137b2be195c34e
Alex <alex@xplace.com>
2017-01-12 18:10:19 +0200

    red2 config wip

--- src/main/java/com.zalizniak/Main.java
+++
@@ -1,7 +0,0 @@
-package com.zalizniak;
-
-/**
- * Created by aillusions on 1/12/2017.
- */
-public class Main {
-}
---
+++ src/main/java/com.zalizniak/MainRed2.java
@@ -0,0 +1,9 @@
+package com.zalizniak;
+
+import org.red5.server.adapter.MultiThreadedApplicationAdapter;
+
+/**
+ * Created by aillusions on 1/12/2017.
+ */
+public class MainRed2 extends MultiThreadedApplicationAdapter {
+}
